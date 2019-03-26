21eee317446292e53daba7f81b36bb187b97c77b
Minh Hoang <minhhoang97hk@gmail.com>
2019-03-27 00:27:21 +0700

    edit routing

--- ReadComic/ReadComic/Areas/Admin/AdminAreaRegistration.cs
+++ ReadComic/ReadComic/Areas/Admin/AdminAreaRegistration.cs
@@ -18,191 +18,191 @@ namespace ReadComic.Areas.Admin
             //chu kỳ phát hành
             context.Routes.MapHttpRoute(
                 "APIDanhSachChuKyPhatHanh",
-                "api/admin/danh-sach-chu-ky",
+                "frequencies",
                 new { controller = "QuanLyChuKyTruyen", action = "DanhSachChuKyPhatHanh", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIChuKyPhatHanh",
-                "api/admin/chu-ky/{id}",
+                "frequencies/{id}",
                 new { controller = "QuanLyChuKyTruyen", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteChuKyPhatHanh",
-                "api/admin/delete/chu-ky/{id}",
+                "frequencies/{id}",
                 new { controller = "QuanLyChuKyTruyen", action = "DeleteChuKyTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APICreateChuKyPhatHanh",
-                "api/admin/create/chu-ky",
+                "frequencies",
                 new { controller = "QuanLyChuKyTruyen", action = "ThemChuKy", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateChuKyPhatHanh",
-                "api/admin/update/chu-ky",
+                "frequencies/{id}",
                 new { controller = "QuanLyChuKyTruyen", action = "UpdateChuKy", id = UrlParameter.Optional }
             );
 
             //loại truyện
             context.Routes.MapHttpRoute(
                 "APIDanhSachLoaiTruyen",
-                "api/admin/danh-sach-loai-truyen",
+                "categorys",
                 new { controller = "QuanLyLoaiTruyen", action = "DanhSachLoaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APILoaiTruyenPhatHanh",
-                "api/admin/loai-truyen/{id}",
+                "categorys/{id}",
                 new { controller = "QuanLyLoaiTruyen", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteLoaiTruyen",
-                "api/admin/delete/loai-truyen/{id}",
+                "categorys/{id}",
                 new { controller = "QuanLyLoaiTruyen", action = "DeleteLoaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APICreateLoaiTruyen",
-                "api/admin/create/loai-truyen",
+                "categorys",
                 new { controller = "QuanLyLoaiTruyen", action = "ThemLoaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateLoaiTruyen",
-                "api/admin/update/loai-truyen",
+                "categorys/{id}",
                 new { controller = "QuanLyLoaiTruyen", action = "UpdateLoaiTruyen", id = UrlParameter.Optional }
             );
 
             //nhóm dịch----------------------------------------------------------------------------------------
             context.Routes.MapHttpRoute(
                 "APIDanhSachNhomDich",
-                "api/admin/danh-sach-nhom-dich",
+                "teams",
                 new { controller = "QuanLyNhomDich", action = "DanhSachNhomDich", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APINhomDich",
-                "api/admin/nhom-dich/{id}",
+                "teams/{id}",
                 new { controller = "QuanLyNhomDich", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteNhomDich",
-                "api/admin/delete/nhom-dich/{id}",
+                "teams/{id}",
                 new { controller = "QuanLyNhomDich", action = "DeleteNhomDich", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APICreateNhomDich",
-                "api/admin/create/nhom-dich",
+                "teams",
                 new { controller = "QuanLyNhomDich", action = "ThemNhomDich", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateNhomDich",
-                "api/admin/update/nhom-dich",
+                "teams/{id}",
                 new { controller = "QuanLyNhomDich", action = "UpdateNhomDich", id = UrlParameter.Optional }
             );
 
             //tác giả  ----------------------------------------------------------------------------------------
             context.Routes.MapHttpRoute(
                 "APIDanhSachTacGia",
-                "api/admin/danh-sach-tac-gia",
+                "authors",
                 new { controller = "QuanLyTacGia", action = "DanhSachTacGia", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APITacGia",
-                "api/admin/tac-gia/{id}",
+                "authors/{id}",
                 new { controller = "QuanLyTacGia", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteTacGia",
-                "api/admin/delete/tac-gia/{id}",
+                "authors/{id}",
                 new { controller = "QuanLyTacGia", action = "DeleteTacGia", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APICreateTacGia",
-                "api/admin/create/tac-gia",
+                "authors",
                 new { controller = "QuanLyTacGia", action = "ThemTacGia", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateTacGia",
-                "api/admin/update/tac-gia",
+                "authors/{id}",
                 new { controller = "QuanLyTacGia", action = "UpdateTacGia", id = UrlParameter.Optional }
             );
 
             //tài khoản  ----------------------------------------------------------------------------------------
             context.Routes.MapHttpRoute(
                 "APIDanhSachTaiKhoan",
-                "api/admin/danh-sach-tai-khoan",
+                "account",
                 new { controller = "QuanLyTaiKhoan", action = "DanhSachTacGia", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APITaiKhoan",
-                "api/admin/tai-khoan/{id}",
+                "account/{id}",
                 new { controller = "QuanLyTaiKhoan", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteTaiKhoan",
-                "api/admin/delete/tai-khoan/{id}",
+                "account/{id}",
                 new { controller = "QuanLyTaiKhoan", action = "DeleteTaiKhoan", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateTrangThai",
-                "api/admin/update/trang-thai",
+                "account/status/{id}",
                 new { controller = "QuanLyTaiKhoan", action = "UpdateTrangThaiTaiKhoan", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateTaiKhoan",
-                "api/admin/update/tai-khoan",
+                "account/{id}",
                 new { controller = "QuanLyTaiKhoan", action = "UpdateTaiKhoan", id = UrlParameter.Optional }
             );
             context.Routes.MapHttpRoute(
                 "APIUpdateNhom",
-                "api/admin/update/nhom",
+                "account/nhom/{id}",
                 new { controller = "QuanLyTaiKhoan", action = "UpdateNhomTaiKhoan", id = UrlParameter.Optional }
             );
 
             //trạng thái truyện  ----------------------------------------------------------------------------------------
             context.Routes.MapHttpRoute(
                 "APIDanhSachTrangThaiTruyen",
-                "api/admin/danh-sach-trang-thai-truyen",
+                "sstatus",
                 new { controller = "QuanLyTrangThaiTruyen", action = "DanhSachTrangThaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APITrangThaiTruyen",
-                "api/admin/trang-thai-truyen/{id}",
+                "sstatus/{id}",
                 new { controller = "QuanLyTrangThaiTruyen", action = "Get", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIDeleteTrangThaiTruyen",
-                "api/admin/delete/trang-thai-truyen/{id}",
+                "sstatus/{id}",
                 new { controller = "QuanLyTrangThaiTruyen", action = "DeleteTrangThaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APICreateTrangThaiTruyen",
-                "api/admin/create/trang-thai-truyen",
+                "sstatus",
                 new { controller = "QuanLyTrangThaiTruyen", action = "ThemTrangThaiTruyen", id = UrlParameter.Optional }
             );
 
             context.Routes.MapHttpRoute(
                 "APIUpdateTrangThaiTruyen",
-                "api/admin/update/trang-thai-truyen",
+                "sstatus/{id}",
                 new { controller = "QuanLyTrangThaiTruyen", action = "UpdateTrangThaiTruyen", id = UrlParameter.Optional }
             );
 
